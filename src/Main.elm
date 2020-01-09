9abff9ec913196b256a33ed0cc8726264bb771d3
Gustavo Martins <martins.yo@gmail.com>
2020-01-09 23:14:41 +0100

    added styling and formatting

--- src/Main.elm
+++ src/Main.elm
@@ -4,53 +4,71 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Http
-import Time
-import Json.Decode exposing (list, string, succeed, fail)
+import Json.Decode exposing (fail, list, string, succeed)
 import Json.Decode.Pipeline exposing (required)
+import Time
+
 
 
 ---- MODEL ----
 
 
-type alias Model = {senhas: List Senha}
+type alias Model =
+    { senhas : List Senha }
 
-type alias Senha = {codigo: String, status: Estado}
 
-type Estado = Emitida | Encaminhada | EmAtendimento | Finalizada
+type alias Senha =
+    { codigo : String, status : Estado }
+
+
+type Estado
+    = Emitida
+    | Encaminhada
+    | EmAtendimento
+    | Finalizada
 
 
 senhaDecoder : Json.Decode.Decoder Senha
 senhaDecoder =
     Json.Decode.succeed Senha
-    |> Json.Decode.Pipeline.required "codigo" string
-    |> Json.Decode.Pipeline.required "status" statusDecoder
+        |> Json.Decode.Pipeline.required "codigo" string
+        |> Json.Decode.Pipeline.required "status" statusDecoder
+
 
 statusDecoder : Json.Decode.Decoder Estado
 statusDecoder =
-    Json.Decode.string |>
-    Json.Decode.andThen (\str ->
-        case str of
-           "EMITIDA" -> 
-               Json.Decode.succeed Emitida
-           "ENCAMINHADA" -> 
-               Json.Decode.succeed Encaminhada
-           "EMATENDIMENTO" -> 
-               Json.Decode.succeed EmAtendimento
-           "FINALIZADA" -> 
-               Json.Decode.succeed Finalizada
-        
-           _ ->
-               Json.Decode.fail "Nao consegui decodar a senha"
-        )
+    Json.Decode.string
+        |> Json.Decode.andThen
+            (\str ->
+                case str of
+                    "EMITIDA" ->
+                        Json.Decode.succeed Emitida
+
+                    "ENCAMINHADA" ->
+                        Json.Decode.succeed Encaminhada
+
+                    "EMATENDIMENTO" ->
+                        Json.Decode.succeed EmAtendimento
+
+                    "FINALIZADA" ->
+                        Json.Decode.succeed Finalizada
+
+                    _ ->
+                        Json.Decode.fail "Nao consegui decodar a senha"
+            )
+
+
 init : ( Model, Cmd Msg )
 init =
-    ( {senhas = []}, getEntries )
+    ( { senhas = [] }, getEntries )
+
+
 
 ---- UPDATE ----
 
 
-type Msg =
-    NewEntries (Result Http.Error (List Senha))
+type Msg
+    = NewEntries (Result Http.Error (List Senha))
     | Tick Time.Posix
 
 
@@ -59,18 +77,20 @@ update msg model =
     case msg of
         NewEntries (Ok jsonString) ->
             let
-                _= Debug.log "It worked! " jsonString
+                _ =
+                    Debug.log "It worked! " jsonString
             in
-                ({model | senhas = jsonString}, Cmd.none)
+            ( { model | senhas = jsonString }, Cmd.none )
 
         NewEntries (Err error) ->
             let
-                _ = Debug.log "Oops, not as we wished! " error
+                _ =
+                    Debug.log "Oops, not as we wished! " error
             in
-                (model, Cmd.none)
+            ( model, Cmd.none )
 
         Tick _ ->
-            (model, getEntries)
+            ( model, getEntries )
 
 
 
@@ -80,52 +100,65 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ 
-        viewSenha model Encaminhada "Senhas solicitadas"
+        [ viewSenha model Encaminhada "Senhas solicitadas"
         , viewSenha model EmAtendimento "Senhas em atendimento"
         , viewSenha model Finalizada "Senhas finalizadas"
         , viewSenhas model
         ]
 
+
 viewSenhas : Model -> Html Msg
 viewSenhas model =
-    div [] [
-            text "Todas as senhas"
-        , div [] [
-                    div []
-        (List.map (\senha -> p [] [text senha.codigo]) model.senhas)
-                ]
-    ]
-
-viewSenha : Model -> Estado-> String -> Html Msg
-viewSenha model estado texto=
-    div [] [
-            text texto
-            , div [] [
-                        div []
-                            (List.map 
-                                (\senha -> 
-                                    if senha.status == estado then
-                                        p [] [text senha.codigo]
-                                    else
-                                        text ""
-                                ) 
-                                model.senhas)
-                    ]
-    ]
+    div []
+        [ text "Todas as senhas"
+        , div []
+            [ div []
+                (List.map (\senha -> p [] [ text senha.codigo ]) model.senhas)
+            ]
+        ]
 
 
----- COMMANDS ---    
+viewSenha : Model -> Estado -> String -> Html Msg
+viewSenha model estado texto =
+    div []
+        [ text texto
+        , div []
+            [ div []
+                (List.map
+                    (\senha ->
+                        if senha.status == estado then
+                            p [] [ text senha.codigo ]
+
+                        else
+                            text ""
+                    )
+                    model.senhas
+                )
+            ]
+        ]
+
+
+
+---- COMMANDS ---
+
 
 getEntries : Cmd Msg
-getEntries = 
-    Http.get{
-        url = "http://localhost:8080/senhas"
-        ,expect = Http.expectJson NewEntries (Json.Decode.list senhaDecoder)}
+getEntries =
+    Http.get
+        { url = "http://localhost:8080/senhas"
+        , expect = Http.expectJson NewEntries (Json.Decode.list senhaDecoder)
+        }
+
+
+
 ---- SUBSCRPTIONS ----
+
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Time.every 2000 Tick
+
+
 
 ---- PROGRAM ----
 
